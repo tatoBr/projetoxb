4bcb30c979d86ce25e0459222350d53cc9c331b6
tatoBr <tato.l.santos@gmail.com>
2021-02-09 15:16:10 -0300

    removidas rotas desnecessárias de app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const ejs = require( 'ejs' );
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+//const usersRouter = require('./routes/users');
 const clientesRouter = require( './routes/clientes' );
 const conteudoRouter = require( './routes/conteudo' );
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 app.use('/clientes', clientesRouter );
 app.use( '/conteudo', conteudoRouter );
 
