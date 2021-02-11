8edc8bcab8215f3b4c037e5de8d7f340cf5c5726
tatoBr <tato.l.santos@gmail.com>
2021-02-11 14:01:48 -0300

    adicionadas rotas para atualizar conteúdo e inserir itens na timeline

--- controllers/conteudo.js
+++ controllers/conteudo.js
@@ -29,7 +29,9 @@ module.exports = {
             clientes[ index ].conteudo.push({
                 id: idconteudo,
                 titulo: titulo,
-                timeline: [{ descricao: detalhes, date: new Date()}],
+                descricao: detalhes,
+                timeline: [{ data: new Date(), detalhes: `Primeira amostra do conteúdo ${ titulo } foi criada.`}],
+
                 status: STATUS_CONTEUDO.EM_ANALISE
             });
             fsp.writeFile( clientesDataPath, JSON.stringify( clientes ), 'utf-8' )
@@ -68,7 +70,91 @@ module.exports = {
         .catch( err => res.status( 500 ).json({ response: `Erro ao ler arquivo: ${ err.message }`}));        
     },
 
+    update_conteudo: async ( req, res )=>{
+        const { clienteId, conteudoId } = req.params
+        const { titulo, descricao } = req.body
+
+        if( [clienteId, conteudoId, titulo, descricao ].includes( undefined ))
+            return res.status( 400 ).json({ response:"Existem dados inválidos na sua requisição."});
+
+        fsp.readFile( clientesDataPath, 'utf-8' )
+        .then( data => {
+            let clientes = null;
+            
+            try { clientes = JSON.parse( data )}
+            catch ( err ) { clientes= []}
+
+            if( !Array.isArray( clientes )) clientes = [];
+
+            const clienteIndex = clientes.findIndex( cliente => cliente.id == clienteId );            
+            if( clienteIndex < 0 ) return res.status( 404 ).json({ response: `Cliente com id ${ clienteId } não encontrado.`});
+
+            const conteudoIndex = clientes[ clienteIndex ].conteudo.findIndex( conteudo => conteudo.id == conteudoId )
+            if( conteudoIndex < 0 ) return res.status( 404 ).json({ response: `Conteúdo com id ${ conteudoId } não encontrado.`});
+
+            clientes[clienteIndex].conteudo[ conteudoIndex ].titulo = titulo;
+            clientes[clienteIndex].conteudo[ conteudoIndex ].descricao = descricao;
+
+            fsp.writeFile( clientesDataPath, JSON.stringify( clientes ), 'utf-8' )
+            .then(() => res.status( 202 ).redirect( `/clientes/detalhes/${ clienteId }` ))
+            .catch(  err => res.status( 500 ).json({ response: `Erro ao salvar arquivo: ${ err.message }`}))
+        })
+        .catch(  err => res.status( 500 ).json({ response: `Erro ao Ler arquivo: ${ err.message }`}))        
+    },
+
     delete_conteudo: ( req, res )=>{
-        res.send( 'rota funcionando' );  
+        const { clienteId, conteudoId } = req.params;
+        fsp.readFile( clientesDataPath, 'utf-8')
+        .then( data => {
+            let clientes = null;
+            
+            try { clientes = JSON.parse( data )}
+            catch ( err ) { clientes= []}
+
+            if( !Array.isArray( clientes )) clientes = [];
+
+            const clienteIndex = clientes.findIndex( cliente => cliente.id == clienteId );            
+            if( clienteIndex < 0 ) return res.status( 404 ).json({ response: `Cliente com id ${ clienteId } não encontrado.`});
+
+            const conteudoIndex = clientes[ clienteIndex ].conteudo.findIndex( conteudo => conteudo.id == conteudoId )
+            if( conteudoIndex < 0 ) return res.status( 404 ).json({ response: `Conteúdo com id ${ conteudoId } não encontrado.`});
+
+            clientes[clienteIndex].conteudo.splice( conteudoIndex, 1 );
+            fsp.writeFile( clientesDataPath, JSON.stringify( clientes ), 'utf-8')
+            .then(() => res.status( 202 ).json({ response: clientes }))
+            .catch( err => res.status( 500 ).json({ response: `Erro ao salvar arquivo: ${ err.message }`}))
+        })
+        .catch( err => res.status( 500 ).json({ response: `Erro ao ler arquivo: ${ err.message }`}))
+    },
+
+    add_to_timeline: async ( req, res ) => {
+        const { clienteId, conteudoId } = req.params
+        const { detalhes } = req.body;
+
+        if( [ clienteId, conteudoId, detalhes ].includes( undefined ))
+            return res.status( 400 ).json({ response:"Existem dados inválidos na sua requisição."});
+
+        fsp.readFile( clientesDataPath, 'utf-8' )
+        .then( data => {
+            let clientes = null;
+            
+            try { clientes = JSON.parse( data )}
+            catch ( err ) { clientes= []}
+
+            if( !Array.isArray( clientes )) clientes = [];
+
+            const clienteIndex = clientes.findIndex( cliente => cliente.id == clienteId );            
+            if( clienteIndex < 0 ) return res.status( 404 ).json({ response: `Cliente com id ${ clienteId } não encontrado.`});
+
+            const conteudoIndex = clientes[ clienteIndex ].conteudo.findIndex( conteudo => conteudo.id == conteudoId )
+            if( conteudoIndex < 0 ) return res.status( 404 ).json({ response: `Conteúdo com id ${ conteudoId } não encontrado.`});
+
+            clientes[clienteIndex].conteudo[ conteudoIndex ].timeline.push({ data: new Date(), detalhes: detalhes });           
+
+            fsp.writeFile( clientesDataPath, JSON.stringify( clientes ), 'utf-8' )
+            .then(() => res.status( 202 ).redirect( `/clientes/detalhes/${ clienteId }` ))
+            .catch(  err => res.status( 500 ).json({ response: `Erro ao salvar arquivo: ${ err.message }`}))
+        })
+        .catch(  err => res.status( 500 ).json({ response: `Erro ao Ler arquivo: ${ err.message }`}))
     }
 }
--- routes/conteudo.js
+++ routes/conteudo.js
@@ -9,7 +9,13 @@ router.post('/add/:clienteId', controller.post_conteudo );
 // GET >> conteudo/detalhes
 router.get( '/detalhes/:clienteId/:conteudoId', controller.get_conteudo );
 
+// PUT >> conteudo/update
+router.put( '/update/:clienteId/:conteudoId', controller.update_conteudo );
+
 // Delete >> conteudo/delete
 router.delete( '/delete/:clienteId/:conteudoId', controller.delete_conteudo)
+
+// PUT >> conteudo/timeline/add
+router.put( '/timeline/add/:clienteId/:conteudoId', controller.add_to_timeline );
 
 module.exports = router;
