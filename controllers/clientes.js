d06461f29cc6b2effc65d669a61b1506043f6e8b
tatoBr <tato.l.santos@gmail.com>
2021-02-11 10:22:50 -0300

    adicionado controller para deletar cliente

--- controllers/clientes.js
+++ controllers/clientes.js
@@ -5,16 +5,20 @@ const path = require( 'path' )
 const clientesDataPath = path.join( 'data', 'cliente.json' );
 const STATUS_CONTEUDO = { APROVADO: 0, EM_ANALISE: 2, REPROVADO: 3 };
 
-
-
 module.exports = {
     get_clientes: async ( req, res )=>{         
-        let lerClientes = fsp.readFile( clientesDataPath, 'utf-8' )
-            .then( data => res.status( 200 ).render( 'clientes', { title: 'Clientes', clientes: JSON.parse( data )}))        
-            .catch( err => {                
-                if( err.code !== 'ENOENT') return res.status( 200 ).render( 'clientes', { title: 'Clientes', clientes: []}) 
-                else return res.status( 200 ).render( 'clientes', { title: 'Clientes', clientes: []});                
-        
+        fsp.readFile( clientesDataPath, 'utf-8' )
+        .then( data => {
+            let parsedData = null;
+
+            try{ parsedData = JSON.parse( data )}
+            catch( err ){ parsedData = [] }
+            
+            return res.status( 200 ).json({ response: parsedData });
+        })        
+        .catch( err => {                
+            if( err.code !== 'ENOENT') return res.status( 200 ).json({ response: [] })
+            else return res.status( 500 ).json({ response: `Erro ao ler arquivo: ${ err.message }`});     
         });     
     },
 
@@ -22,11 +26,14 @@ module.exports = {
         if( !fs.existsSync( path.join( 'data'))){
             fsp.mkdir( path.join( 'data' ))
             .then(()=> console.log( 'diretorio foi criado'))
-            .catch( err =>  res.status( 500 ).send( 'Erro ao Cria diretório: ' + err.message ));          
+            .catch( err =>  res.status( 500 ).json({ response: 'Erro ao Cria diretório: ' + err.message }));          
         }
 
         const { nome } = req.body;
         const id = Math.floor( 10000 + Math.random() * 89999 );
+        
+        if( !nome ) return res.status( 400 ).json({ response:"Existem dados inválidos na sua requisição."})
+
         let clientes = null;
         let cliente = { id: id, nome: nome, conteudo: [] };
 
@@ -35,65 +42,66 @@ module.exports = {
             try{ clientes = JSON.parse( data )}
             catch( err ) { clientes = []}
 
-            if( Array.isArray( clientes )) clientes.push( cliente )
-            else{
-                clientes = [];
-                clientes.push( cliente );
-            }
+            if( !Array.isArray( clientes )) clientes = [];                           
+            
+            clientes.push( cliente );           
 
             fsp.writeFile( clientesDataPath, JSON.stringify( clientes ))
             .then(()=> res.status( 202 ).redirect( '/clientes' ))
-            .catch( err => res.status( 500 ).send( 'Erro ao Salvar Arquivo: ' + err.message ));
-        })
+            .catch( err => res.status( 500 ).json({ response: `Erro salvando arquivo: ${ err.message }`})
+            )}
+        )
         .catch( err => {
-            if( err.code === 'ENOENT'){
-                clientes = [] 
-                clientes.push( cliente );
+            if( err.code === 'ENOENT'){                
+                clientes = [ cliente ]               
                 fsp.writeFile( clientesDataPath, JSON.stringify( clientes ))
                 .then(()=> res.status( 202 ).redirect( '/clientes' ))
-                .catch( err => res.status( 500 ).send( 'Erro ao Salvar Arquivo: ' + err.message ));
+                .catch( err => res.status( 500 ).json({ response: `Erro salvando arquivo: ${ err.message }`}));
             }
-            else return res.status( 500 ).send( 'Erro ao Salvar Arquivo: ' + err.message )
+            else return res.status( 500 ).json({ response: `Erro salvando arquivo: ${ err.message }`})
         });
-    },    
-
-    get_detalhes: async ( req, res )=>{
-        let cliente_id = req.query.id;
-        fsp.readFile( clientesDataPath, 'utf-8' )
-        .then( data => {
-            try{ clientes = JSON.parse( data )}
-            catch( err ) { clientes = []}
-            let index = clientes.findIndex( cliente => cliente.id == cliente_id );
-            return res.status( 200 ).render( 'detalhes-cliente', {
-                title: `Detalhes do Cliente ${ clientes[ index ].nome }`,
-                cliente: clientes[ index ],
-                index: index
-            });
-        })
-        .catch( err => res.status( 500 ).send( 'erro ao ler arquivo. ' + err.message ))        
     },
 
-    post_conteudo: async( req, res )=>{
-        const { id: cliente_id, titulo, detalhes } = req.body;
-        const idconteudo = Math.floor( Math.random() * 999_999 );
+    delete_cliente: async ( req, res )=>{
+        const { clienteId } = req.params;
+
+        fsp.readFile( clientesDataPath, 'utf-8' )
+        .then( data => {
+            let clientes = null
+            try{ clientes = JSON.parse( data )}
+            catch( err ){ return res.status( 500 ).json({ response: 'Erro ao converter json.' + err.message })};
+            
+            
+            const index = clientes.findIndex( cliente => cliente.id == clienteId );
+
+            if( index >= 0 ){
+                clientes.splice( index, 1 );
+                fsp.writeFile( clientesDataPath, JSON.stringify( clientes ))
+                .then(() => res.status( 200 ).json({ response: `Cliente com id ${ clienteId } apagado com sucesso` }))
+                .catch( err => res.status( 500 ).json({ response: `Erro ao salvar arquivo. \nCódigo:${ err.code }\nMensagem:${ err.message }`}))
+            }
+            else{
+                console.log( clienteId )
+                return res.status( 404 ).json({ response: `Cliente com id ${ clienteId } não encontrado.`})
+            }            
+        })
+        .catch( err => res.status( 500 ).json({ response: `Erro ao ler arquivo: ${ err.message }`}));
+    },   
+
+    get_detalhes: async ( req, res )=>{
+        let { clienteId } = req.params;
+        let clientes = null;
+
         fsp.readFile( clientesDataPath, 'utf-8' )
         .then( data => {
             try{ clientes = JSON.parse( data )}
             catch( err ) { clientes = []}
 
-            let index = clientes.findIndex( cliente => cliente.id == cliente_id );
-            if( index < 0 ) return res.status( 404 ).send( 'usuario não encontrado' );
+            let index = clientes.findIndex( cliente => cliente.id == clienteId );
 
-            clientes[ index ].conteudo.push({
-                id: idconteudo,
-                titulo: titulo,
-                detalhes: [{ detalhes: detalhes, date: new Date()}],
-                status: STATUS_CONTEUDO.EM_ANALISE
-            });
-            fsp.writeFile( clientesDataPath, JSON.stringify( clientes ))
-            .then(() => res.status( 202 ).redirect( '/clientes' ))
-            .catch( err => res.status( 500 ).send( 'Erro ao Salvar Arquivo: ' + err.message ));
+            if( index < 0 ) return res.status( 404 ).json({ response: `Cliente com id ${ clienteId } não encontrado.` })
+            return res.status( 200 ).json({ response: clientes[ index ]});
         })
-        .catch( err => res.status( 500 ).send( 'erro ao ler arquivo. ' + err.message ));        
+        .catch( err => res.status( 500 ).json({ response: `Erro ao ler arquivo: ${ err.message }`}))   
     }
 }
